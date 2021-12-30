b966a3d41dd59af881c656c8a4769dbd690473c2
Flandre Cirno <35405826+FlandreCirno@users.noreply.github.com>
2021-12-30 12:16:12 -0500

    new page

---
+++ js/charData.js
@@ -0,0 +1,8 @@
+var charData = {
+	"siren_ship" : {
+		"name" : "Siren Ship",
+		"type" : "Miscellaneous",
+		"group" : "Miscellaneous",
+		"skin" : ["sairen10_danchuan", "sairenboss10", "sairenhangmu_6", "sairenqianting_6", "sairenqingxun_6", "sairenquzhu_6", "sairenzhanlie_6", "sairenzhongxun_6", "shenyuanboss7", "srBB6", "srCA6", "srCL6", "srCV6", "srDD6", "srfeiji6", "srSS6"]
+    },
+};
